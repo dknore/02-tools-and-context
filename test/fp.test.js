a9af9eabe9bccfc20927cde644f60fdd09b4b0b2
dknore <darcy.knore@gmail.com>
2018-02-08 19:57:45 -0800

    adding test.js file

---
+++ test/fp.test.js
@@ -0,0 +1,6 @@
+"use strict";
+
+const forEach = require('../lib/fp.js').forEach;
+const map = require('../lib/fp.js').map;
+const filter = require('../lib/fp.js').filter;
+const reduce = require('../lib/fp.js').reduce;
